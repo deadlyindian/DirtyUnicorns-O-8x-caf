3749c059c308dd2107166a3a7582b8c83fb7248c
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2016-09-18 22:33:08 +0200

    Add Sound tile to Quick Settings
    abc ezio84: adapt to O, show volume panel with long press,
    use full dnd (no interruptions) as silent mode
    
    @beanstown106: Sound Tile improvements
    *added longpress action
    *added more descriptive labels for each state instead of always saying sound
    
    Change-Id: I4b837d8758050dede03d34eee8083bbb8fbb0baa
    ezio84: Adapt for N

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -79,6 +80,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("nfc")) return new NfcTile(mHost);
         else if (tileSpec.equals("reboot")) return new RebootTile(mHost);
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(mHost);
+        else if (tileSpec.equals("sound")) return new SoundTile(mHost);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(mHost);
         else if (tileSpec.equals("navigation")) return new NavigationBarTile(mHost);
         else if (tileSpec.equals("theme")) return new ThemeTile(mHost);
